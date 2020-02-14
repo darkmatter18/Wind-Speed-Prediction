980b8f0a3a36e890f41e0ba15373fc893dc06e92
Arkadip <in2arkadipb13@gmail.com>
2020-02-14 23:24:29 +0530

    Added cuda support, more housekeeping on the notebook

--- model.py
+++ model.py
@@ -12,6 +12,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -30,12 +31,12 @@ class Model(nn.Module):
         return x
     
     
-    def fit(self, trainloader, validationloader, loss, optim, lr, epochs, val_per_batch, cuda):
+    def fit(self, trainloader, validationloader, loss, optim, lr, epochs, val_per_batch, cuda=False):
         trainlosses = []
         testlosses = []
 
-        criterion = loss()
-        optimizer = optim(params=self.parameters(), lr=lr)
+        self.criterion = loss()
+        self.optimizer = optim(params=self.parameters(), lr=lr)
         
         for epoch in range(epochs):
             trainloss = 0
@@ -44,15 +45,15 @@ class Model(nn.Module):
                 data = data.type(torch.FloatTensor)
                 target = target.type(torch.FloatTensor)
                 if cuda:
-                    image, target = image.cuda(), target.cuda()
+                    data, target = data.cuda(), target.cuda()
         
-                optimizer.zero_grad()
+                self.optimizer.zero_grad()
         
                 output = self.forward(data)
-                loss = criterion(output, target)
+                loss = self.criterion(output, target)
         
                 loss.backward()
-                optimizer.step()
+                self.optimizer.step()
         
                 trainloss += loss.item()
         
@@ -64,27 +65,51 @@ class Model(nn.Module):
                             data = data.type(torch.FloatTensor)
                             target = target.type(torch.FloatTensor)
                             if cuda:
-                                image, target = image.cuda(), target.cuda()
+                                data, target = data.cuda(), target.cuda()
                             ps = self.forward(data)
-                            testloss += criterion(ps, target).item()
+                            testloss += self.criterion(ps, target).item()
                         testloss = testloss / len(validationloader)        
                     trainloss = trainloss / len(trainloader)
         
                 trainlosses.append(trainloss)
                 testlosses.append(testloss)
         
-                print(f'Epoch: {epoch}',
+                print(f'Epoch: {epoch+1}',
                       f'Batch: {batch} out of {len(trainloader)}',
                       f'Training Loss: {trainloss}',
                       f'Test Loss: {testloss}')
+        self.trainlosses = trainlosses
+        self.testlosses = testlosses
         return (trainlosses, testlosses)
     
-    def test(self, testloader):
+    def test(self, testloader, cuda=False):
         result = []
         expected = []
         for data, label in testloader:
-            for res in self.forward(data.type(torch.FloatTensor)).tolist():
+            data = data.type(torch.FloatTensor)
+            if cuda:
+                data, label = data.cuda(), label.cuda()
+            for res in self.forward(data).tolist():
                 result.append(res[0])
             for expt in label.tolist():
                 expected.append(expt[0])
+        
+        self.result = result
+        self.expected = expected
         return (result, expected)
+    
+    def save(self, path, save_optim=False, cuda=False):
+        try:
+            os.mkdir(path)
+            print(path, " - dir Created")
+        except FileExistsError:
+            print(path, " - dir Already exists")
+        finally:
+            NAME = 'model_cuda.pt' if cuda else 'model_cpu.pt'
+            torch.save(self.state_dict(), os.path.join(path, NAME))
+            print('Model saved in', path)
+            if save_optim:
+                NAME = 'optim_cuda.pt' if cuda else 'optim_cpu.pt'
+                torch.save(self.optimizer.state_dict(), os.path.join(path, NAME))
+                print('Optimizer saved in', path)
+        return
